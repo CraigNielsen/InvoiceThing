33c8c5e82a3e4e25f68bed32a92ef0d2bc504df1
Craig Neilsen <craig@clbnLinux.clbndomain>
2015-12-22 19:30:54 +0200

    fixed for python3

--- backend/api/v1/resources/users/user_delete.py
+++ backend/api/v1/resources/users/user_delete.py
@@ -13,7 +13,7 @@ class Users_delete(Resource):
     def post(self):
         args = parser.parse_args()
         di = json.loads(args.data)
-        for n, v in di.iteritems():
+        for n, v in di.items():
             temp = User.query.filter(User.id == n).one()
             db.session.delete(temp)
         db.session.commit()
