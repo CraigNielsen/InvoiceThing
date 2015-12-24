a94d89f90312ffd8a0c4aa847828b3f4e915a351
Jason Russell <jasrusable@gmail.com>
2015-12-24 15:51:27 +0200

    Improve formatting

--- www/src/controller.js
+++ www/src/controller.js
@@ -1,71 +1,63 @@
-
-myApp.controller('appController', function ($scope, $http,$location) {
-	$scope.message="Invoice Pal";
-
+myApp.controller('appController', function ($scope, $http, $location) {
 	updateUsers = function () {
 		getUsers = $http({
 			method: 'GET',
 			url: 'http://localhost:5000/api/v1/users'
 		});
-
 		getUsers.then(function (response) {
 			$scope.users = response.data;
 		});
+	};
 	
-	}
-	updateUsers();
 	changeView = function(view){
             $location.path(view); // path not hash
-        }
-    console.log("adding some spice");
-    idSelectedUser = {};
+    };
+    
 	$scope.setSelected = function (id) {
 		if (id in idSelectedUser){
 			delete idSelectedUser[id];
-		}else{
-			idSelectedUser[id]=id;
+		} else {
+			idSelectedUser[id] = id;
 		}
+	};
 
-};
 	$scope.checkSelected = function (id) {
 		if (id in idSelectedUser){
 			// console.log( id + " is selected");
 			return true;
-
-		}else {
+		} else {
 			// console.log("not selected");
 			return false;
 		}
 	}
+
 	$scope.addUser = function (){
-
-
-		postuser= $http({
+		postuser = $http({
 			method: 'POST',
 			url: 'http://localhost:5000/api/v1/users',
 			data:$scope.user
-			});
-
+		});
 		postuser.then(function (response) {
 			// console.log(response);
 			updateUsers();
 		});
-
 	};
-		$scope.deleteSelected= function (){
+	
+	$scope.deleteSelected= function (){
 		// console.log(JSON.stringify(idSelectedUser));
-		deluser= $http({
+		deluser = $http({
 			method: 'POST',
 			url: 'http://localhost:5000/api/v1/users/delete',
 			data:{'data': JSON.stringify(idSelectedUser) } 
-			});
-
+		});
 		deluser.then(function (response) {
 			// console.log("deleted");
-			idSelectedUser={}	;
+			idSelectedUser = {};
 			updateUsers();
 		});
-
 	};
+	$scope.message="Invoice Pal";
+	updateUsers();
+	console.log("adding some spice");
+    idSelectedUser = {};
 });
-  
