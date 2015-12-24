771ce63c151d3fd89d54d20d8d632c7f5e1a8de6
Jason Russell <jasrusable@gmail.com>
2015-12-24 15:43:15 +0200

    Refactor UserSingleton

--- backend/api/v1/resources/users/user_singleton.py
+++ backend/api/v1/resources/users/user_singleton.py
@@ -4,8 +4,13 @@ from backend.models import User
 from backend.api.v1.schemas import UserSchema
 
 
+user_schema = UserSchema()
+
+
 class UserSingleton(Resource):
     def get(self, user_id):
-        user_schema = UserSchema()
-        user = User.query.filter(User.id == user_id).one()
+        user = User.query.filter(User.id == user_id).first()
+        if not user:
+            # TODO: 404
+            raise Exception('User with id: {0} not found.'.format(user_id))
         return user_schema.dump(user).data
