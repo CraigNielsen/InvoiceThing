fbe6388dcd04620601df51638f906d6053bec848
Jason Russell <jasrusable@gmail.com>
2015-12-24 15:39:36 +0200

    Remove old UserSchema

--- backend/api/v1/resources/users/__init__.py
+++ backend/api/v1/resources/users/__init__.py
@@ -1,6 +1,3 @@
-
-from marshmallow import Schema, fields
-
 from backend.api.v1 import v1_api
 from .user_collection import UserCollection
 from .user_singleton import UserSingleton
@@ -10,9 +7,3 @@ from .user_delete import Users_delete
 v1_api.add_resource(UserCollection, '/users')
 v1_api.add_resource(UserSingleton, '/users/<int:user_id>')
 v1_api.add_resource(Users_delete, '/users/delete')
-
-
-class UserSchema(Schema):
-    id = fields.Integer()
-    name = fields.String()
-    email = fields.String()
