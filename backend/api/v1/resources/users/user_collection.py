f81846ef3d46f32e284fdd4346446ae411f989cf
Jason Russell <jasrusable@gmail.com>
2015-12-24 15:41:51 +0200

    Refactor UserCollection

--- backend/api/v1/resources/users/user_collection.py
+++ backend/api/v1/resources/users/user_collection.py
@@ -5,6 +5,8 @@ from backend.models import User
 from backend.api.v1.schemas import UserSchema
 
 
+user_schema = UserSchema()
+
 parser = reqparse.RequestParser()
 parser.add_argument('name')
 parser.add_argument('email')
@@ -12,16 +14,14 @@ parser.add_argument('email')
 
 class UserCollection(Resource):
     def get(self):
-        user_schema = UserSchema()
         users = User.query.all()
         return user_schema.dump(users, many=True).data
 
     def post(self):
-        user_schema = UserSchema()
         args = parser.parse_args()
         user = User()
-        user.name = args['name']
-        user.email = args['email']
+        for key in args.items():
+            setattr(user, key, args[key])
         db.session.add(user)
         db.session.commit()
         return user_schema.dump(user).data
